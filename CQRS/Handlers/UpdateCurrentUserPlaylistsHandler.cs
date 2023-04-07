ba0ca3ecf36b1f335412bd47193fd7d592acbf85
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-04-07 08:58:18 -0500

    add StopWatch to get timing of individual actions

--- CQRS/Handlers/UpdateCurrentUserPlaylistsHandler.cs
+++ CQRS/Handlers/UpdateCurrentUserPlaylistsHandler.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -11,8 +13,9 @@ namespace Playlister.CQRS.Handlers
     /// <summary>
     /// Add or Update the current user's playlists to the db.
     /// </summary>
-    // ReSharper disable once UnusedType.Global
-    public class UpdateCurrentUserPlaylistsHandler : IRequestHandler<UpdateCurrentUserPlaylistsCommand, int>
+
+    public class UpdateCurrentUserPlaylistsHandler
+        : IRequestHandler<UpdateCurrentUserPlaylistsCommand, int>
     {
         private readonly IPlaylistService _playlistService;
 
@@ -25,12 +28,28 @@ namespace Playlister.CQRS.Handlers
         /// <param name="command"></param>
         /// <param name="ct"></param>
         /// <returns>Number of playlists handled.</returns>
-        public async Task<int> Handle(UpdateCurrentUserPlaylistsCommand command, CancellationToken ct)
+        public async Task<int> Handle(
+            UpdateCurrentUserPlaylistsCommand command,
+            CancellationToken ct
+        )
         {
-            ImmutableArray<Playlist> playlists = (await
-                _playlistService.GetCurrentUserPlaylistsAsync(command.AccessToken, ct)!).ToImmutableArray();
+            var sw = Stopwatch.StartNew();
+
+            ImmutableArray<Playlist> playlists = (
+                await _playlistService.GetCurrentUserPlaylistsAsync(command.AccessToken, ct)!
+            ).ToImmutableArray();
+
+            var elapsedMs = sw.Elapsed.TotalMilliseconds;
+            Console.WriteLine(
+                $"\n{DateTime.Now.ToLocalTime()}: It took {elapsedMs} seconds to get the current user's playlists."
+            );
 
             await _playlistService.UpdatePlaylistsAsync(command.AccessToken, playlists, ct);
+
+            sw.Stop();
+            Console.WriteLine(
+                $"\n{DateTime.Now.ToLocalTime()}: It took {sw.Elapsed.TotalMilliseconds - elapsedMs} seconds to update the current user's playlists."
+            );
 
             return playlists.Length;
         }
