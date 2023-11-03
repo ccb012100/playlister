76305256588c273e88f2cfe252bd7cd642f293d6
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-11-03 10:19:01 -0500

    update test script

--- test.sh
+++ test.sh
@@ -7,8 +7,10 @@ else
     term="hammond"
 fi
 
-cargo build &&
-    echo sorted_albums "$term" &&
-    target/debug/playlist-util tsv ~/src/playlist-search/albums/sorted_albums.tsv search "$term" &&
-    echo all_albums "$term" &&
-    target/debug/playlist-util tsv ~/src/playlist-search/albums/all_albums.tsv search --include-playlist-name "$term"
+cargo build || exit 1
+
+echo sorted_albums "$term"
+target/debug/playlist-util tsv ~/src/playlist-search/albums/sorted_albums.tsv search "$term" || exit 1
+
+echo all_albums "$term"
+target/debug/playlist-util tsv ~/src/playlist-search/albums/all_albums.tsv search --include-playlist-name "$term"
