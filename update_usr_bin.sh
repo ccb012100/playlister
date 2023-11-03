eb7ca66299c8f826bb614892a6d0a2daf09dbccf
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-11-03 10:15:51 -0500

    add script to cp release build to ~/bin

---
+++ update_usr_bin.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -Eeou pipefail
+
+scriptdir=$(dirname -- "$(readlink -f -- "$0")")
+cargo build --release || exit 1
+
+util="$HOME"/bin/playlist-util
+
+if [[ -f "$util" ]]; then
+    rm "$util"
+fi
+
+cp "$scriptdir"/target/release/playlist-util "$util"
+
+echo "$util" updated
