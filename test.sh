68cb342426d29209b7460c77280ed5044aecdb7b
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-11-03 10:37:28 -0500

    modify test script to point to ~/bin

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ fi
 cargo build || exit 1
 
 echo sorted_albums "$term"
-target/debug/playlist-util tsv ~/src/playlist-search/albums/sorted_albums.tsv search "$term" || exit 1
+target/debug/playlist-util tsv ~/bin/albums/sorted_albums.tsv search "$term" || exit 1
 
 echo all_albums "$term"
-target/debug/playlist-util tsv ~/src/playlist-search/albums/all_albums.tsv search --include-playlist-name "$term"
+target/debug/playlist-util tsv ~/bin/albums/all_albums.tsv search --include-playlist-name "$term"
