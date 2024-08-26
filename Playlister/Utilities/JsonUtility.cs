8565654b3e50711540c0d5b8d841e20a1cef74c7
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-08-26 16:02:56 -0500

    use enum serialization, built-it snake case serialization policy

--- Playlister/Utilities/JsonUtility.cs
+++ Playlister/Utilities/JsonUtility.cs
@@ -1,14 +1,13 @@
-using System.Globalization;
 using System.Text.Json;
-using EFCore.NamingConventions.Internal;
 using Refit;
 
 namespace Playlister.Utilities
 {
     public static class JsonUtility
     {
-        private static JsonSerializerOptions SnakeCaseSerializerOptions =>
-            new() { PropertyNamingPolicy = SnakeCaseNamingPolicy.Instance };
+        private static readonly JsonSerializerOptions s_prettyPrintOptions = new() { WriteIndented = true };
+
+        private static JsonSerializerOptions SnakeCaseSerializerOptions => new() { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
 
         public static RefitSettings SnakeCaseRefitSettings =>
             new() { ContentSerializer = new SystemTextJsonContentSerializer(SnakeCaseSerializerOptions) };
@@ -19,15 +18,6 @@ namespace Playlister.Utilities
         /// <param name="t">Object you want converted to JSON</param>
         /// <typeparam name="T">Type of <paramref name="t" /></typeparam>
         /// <returns>String representation of the object as pretty-printed JSON</returns>
-        public static string PrettyPrint<T>(T t) =>
-            JsonSerializer.Serialize(t, new JsonSerializerOptions { WriteIndented = true });
-
-        /// <summary>
-        ///     Convert string to snake_case using EFCore's <see cref="SnakeCaseNameRewriter" />
-        /// </summary>
-        /// <param name="s">input string</param>
-        /// <returns>input string converted to snake_case</returns>
-        public static string ConvertToSnakeCase(string s) =>
-            new SnakeCaseNameRewriter(CultureInfo.InvariantCulture).RewriteName(s);
+        public static string PrettyPrint<T>(T t) => JsonSerializer.Serialize(t, s_prettyPrintOptions);
     }
 }
