fc87a666269f02dd0ac75654f0659747ff127551
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-09-12 18:13:03 -0500

    add fixme comment to shell script

--- shell/scripts/generate_playlister_db_schema.sh
+++ shell/scripts/generate_playlister_db_schema.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -eu
-
+# FIXME: this only seems to work if run from the directory "playlister/shell/"; running from "playlister/shell/scripts" just prints to stdout
 scriptdir=$(dirname -- "$(readlink -f -- "$0")")
 sqlite3 --readonly "$HOME/playlister.sqlite3" ".read $scriptdir/../sql/generate_playlister_db_schema.sqlite"
